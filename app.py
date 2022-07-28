074f3eca25a323e2ff5bc18ab266c08c9ad79dee
Rameshber Goswami <81370737+Rameshber@users.noreply.github.com>
2022-07-28 11:48:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def predict():
         
     return render_template('index.html', prediction_text='Regression Model  has predicted House Price On Basis Of SqFt is : {}'.format(prediction))
 
-
-app.run()
+if __name__ == "__main__":
+  app.run(debug = True)
